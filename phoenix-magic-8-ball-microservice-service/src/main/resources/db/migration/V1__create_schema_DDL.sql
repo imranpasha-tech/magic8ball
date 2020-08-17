5468e2ea791f5b3c67df97a9b77f750a676cb852
gsingh <gauravpreet.singh@icims.com>
2020-08-17 17:47:19 +0530

    IMM 1070 Spring boot microservice setup - schema alter

--- phoenix-magic-8-ball-microservice-service/src/main/resources/db/migration/V1__create_schema_DDL.sql
+++ phoenix-magic-8-ball-microservice-service/src/main/resources/db/migration/V1__create_schema_DDL.sql
@@ -1,20 +1,18 @@
-CREATE TABLE magic_eight_ball_languages(
-    locale VARCHAR(8) NOT NULL,
-    name VARCHAR (24) NOT NULL
-) WITH (
-    OIDS = FALSE
-) TABLESPACE pg_default;
+--CREATE TABLE magic_eight_ball_languages(
+--    id integer NOT NULL,
+--    locale VARCHAR(8) NOT NULL,
+--    name VARCHAR (24) NOT NULL,
+--    PRIMARY KEY (id)
+--) WITH (
+--    OIDS = FALSE
+--) TABLESPACE pg_default;
 
 
 ----------
 CREATE TABLE magic_eight_ball_history(
-    id integer NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     question VARCHAR(120) NOT NULL,
     frequency integer NOT NULL,
     language_code VARCHAR(8) NOT NULL,
-    created_date timestamp(6) not null,
-    PRIMARY KEY (id),
-	FOREIGN KEY (language_code) REFERENCES magic_eight_ball_languages (locale)
-) WITH (
-    OIDS = FALSE
+    created_date timestamp(6) not null
 ) TABLESPACE pg_default;
