60ba986e95a023d5d191414a972d7c4ab90a4868
gsingh <gauravpreet.singh@icims.com>
2020-08-19 19:18:55 +0530

    IMM 1070 Spring boot microservice setup - excluding model entity class from sonar scan

--- phoenix-magic-8-ball-microservice-service/src/test/java/com/icims/labs/services/eightball/api/Magic8BallServiceTests.java
+++ phoenix-magic-8-ball-microservice-service/src/test/java/com/icims/labs/services/eightball/api/Magic8BallServiceTests.java
@@ -9,17 +9,12 @@ import com.icims.labs.services.eightball.util.TestUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
