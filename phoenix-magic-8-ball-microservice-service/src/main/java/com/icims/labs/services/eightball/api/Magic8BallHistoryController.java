26db4c7eb1f3c87776fcfedbe99ca21e14437bd8
gsingh <gauravpreet.singh@icims.com>
2020-08-26 23:23:23 +0530

    fix: Removing unused import

--- phoenix-magic-8-ball-microservice-service/src/main/java/com/icims/labs/services/eightball/api/Magic8BallHistoryController.java
+++ phoenix-magic-8-ball-microservice-service/src/main/java/com/icims/labs/services/eightball/api/Magic8BallHistoryController.java
@@ -4,8 +4,6 @@ import com.icims.labs.services.eightball.entity.History;
 import com.icims.labs.services.eightball.service.Magic8BallHistoryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- phoenix-magic-8-ball-microservice-service/src/main/java/com/icims/labs/services/eightball/service/impl/Magic8BallHistoryServiceImpl.java
+++ phoenix-magic-8-ball-microservice-service/src/main/java/com/icims/labs/services/eightball/service/impl/Magic8BallHistoryServiceImpl.java
@@ -3,8 +3,6 @@ package com.icims.labs.services.eightball.service.impl;
 import com.icims.labs.services.eightball.entity.History;
 import com.icims.labs.services.eightball.repository.Magic8BallRepository;
 import com.icims.labs.services.eightball.service.Magic8BallHistoryService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -14,7 +12,7 @@ import java.util.List;
 @Service
 public class Magic8BallHistoryServiceImpl implements Magic8BallHistoryService {
 
-        @Autowired
+    @Autowired
     private Magic8BallRepository magic8BallRepository;
 
     @Override
