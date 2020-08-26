3dd23fb258fa8d02b57536f7853adbc7cf8c9611
Imran Pasha <Imran.Pasha@icims.com>
2020-08-26 19:36:03 +0530

    fix: contorller tests

--- phoenix-magic-8-ball-microservice-service/src/test/java/com/icims/labs/services/eightball/api/Magic8BallControllerTests.java
+++ phoenix-magic-8-ball-microservice-service/src/test/java/com/icims/labs/services/eightball/api/Magic8BallControllerTests.java
@@ -26,104 +26,98 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(Magic8BallController.class)
 public class Magic8BallControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @MockBean
-    private Magic8BallService magic8BallService;
+	@MockBean
+	private Magic8BallService magic8BallService;
 
+	@Test
+	public void verifyStringResultWhenRandomAnswerServiceIsInvoked() throws Exception {
+		SentimentAnswer answer = SentimentAnswer.builder().answer("It is likely").build();
+		when(magic8BallService.getRandomAnswer(buildMockUserRequest())).thenReturn(answer);
 
-    @Test
-    public void verifyStringResultWhenRandomAnswerServiceIsInvoked() throws Exception {
-    	SentimentAnswer answer = SentimentAnswer.builder().answer("It is likely").build();
+		mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(buildMockUserRequest()))).andExpect(status().isOk())
+				.andExpect(jsonPath("$.answer").isString());
+	}
+
+	@Test
+	public void verifyResultWhenRandomAnswerServiceIsInvoked() throws Exception {
+		SentimentAnswer answer = SentimentAnswer.builder().answer("It is likely").build();
         when(magic8BallService.getRandomAnswer(buildMockUserRequest())).thenReturn(answer);
 
-        mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(buildMockUserRequest())))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.answer").isString());
-    }
+		mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(buildMockUserRequest()))).andExpect(status().isOk())
+				.andExpect(jsonPath("$.answer").isString());
+	}
 
-    @Test
-    public void verifyResultWhenRandomAnswerServiceIsInvoked() throws Exception {
-        when(magic8BallService.getRandomAnswer(buildMockUserRequest())).thenReturn("It is likely");
+	@Test
+	public void verifyResultWhenHistoryServiceIsInvoked() throws Exception {
+		when(magic8BallService.getHistory()).thenReturn(TestUtils.buildHistory());
 
-        mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(buildMockUserRequest())))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.answer").isString());
-    }
+		mockMvc.perform(get("/api/history")).andExpect(status().isOk());
 
-    @Test
-    public void verifyResultWhenHistoryServiceIsInvoked() throws Exception {
-        when(magic8BallService.getHistory()).thenReturn(TestUtils.buildHistory());
+	}
 
-        mockMvc.perform(get("/api/history"))
-                .andExpect(status().isOk());
+	@Test
+	public void verifyTrendingQuestionsServiceIsInvoked() throws Exception {
+		when(magic8BallService.getTrendingQuestions(anyString())).thenReturn(TestUtils.buildHistory());
 
-    }
+		mockMvc.perform(
+				get("/api/trendingQuestions").contentType(MediaType.APPLICATION_JSON).param("languageCode", "en-US"))
+				.andExpect(status().isOk());
+	}
 
-    @Test
-    public void verifyTrendingQuestionsServiceIsInvoked() throws Exception {
-        when(magic8BallService.getTrendingQuestions(anyString())).thenReturn(TestUtils.buildHistory());
+	@Test
+	public void verifyTrendingQuestionsServiceIs4XXWhenLanguageIsNotPassed() throws Exception {
+		when(magic8BallService.getTrendingQuestions(anyString())).thenReturn(TestUtils.buildHistory());
 
-        mockMvc.perform(get("/api/trendingQuestions").contentType(MediaType.APPLICATION_JSON).param("languageCode","en-US"))
-                .andExpect(status().isOk());
-    }
+		mockMvc.perform(get("/api/trendingQuestions").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().is4xxClientError());
+	}
 
-    @Test
-    public void verifyTrendingQuestionsServiceIs4XXWhenLanguageIsNotPassed() throws Exception {
-        when(magic8BallService.getTrendingQuestions(anyString())).thenReturn(TestUtils.buildHistory());
+	@Test
+	public void verifyResponseIs4XXWhenRandomAnswerIsInvokedViaGet() throws Exception {
+		mockMvc.perform(get("/api/answer")).andExpect(status().is4xxClientError());
+	}
 
-        mockMvc.perform(get("/api/trendingQuestions").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().is4xxClientError());
-    }
+	public static UserRequest buildMockUserRequest() {
+		Language language = Language.builder().code("en_US").locale("en_US").name("USA").build();
+		return UserRequest.builder().question("Will it rain ?").userId(null).language(language).build();
+	}
 
-    @Test
-    public void verifyResponseIs4XXWhenRandomAnswerIsInvokedViaGet() throws Exception {
-        mockMvc.perform(get("/api/answer")).andExpect(status().is4xxClientError());
-    }
+	@Test
+	public void responseAnswerWhenPayLoadContainsOnlyQuestionMark() throws Exception {
+		mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON).content(
+				"{\"question\":\"?\",\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.answer").value("!"));
+	}
 
-    public static UserRequest buildMockUserRequest() {
-        Language language = Language.builder().code("en_US").locale("en_US").name("USA").build();
-        return UserRequest.builder().question("Will it rain ?").userId(null).language(language).build();
-    }
-    
-    @Test
-    public void responseAnswerWhenPayLoadContainsOnlyQuestionMark() throws Exception
-    {
-    	mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
-    			.content("{\"question\":\"?\",\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
-    	.andExpect(status().isOk())
-    	.andExpect(jsonPath("$.answer").value("!"));
-    }
-    
-  @Test
-    public void responseAnswerWhenPayloadEndsWithQuestionMark() throws Exception
-    {
-    	when(magic8BallService.getRandomAnswer(buildMockUserRequest())).thenReturn("It is likely");
-    	
-    	mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
-    			.content("{\"question\":\"Will it rain today\",\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
-    	        .andExpect(status().isOk());
-    	        
-    }
-   
-  @Test
-  public void responseAnswerWhenPayLoadContainsNull() throws Exception
-  {
-  	mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
-  			.content("{\"question\":null,\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
-  	.andExpect(status().isOk());
-  	
-  }
-  
-  @Test
-  public void responseAnswerWhenPayLoadContainsToomuchLength() throws Exception
-  {
-  	mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON)
-  			.content("{\"question\":\"Will it rain todayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy?\",\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
-  	.andExpect(status().isOk());
-  	
-  }
+	@Test
+	public void responseAnswerWhenPayloadEndsWithQuestionMark() throws Exception {
+		SentimentAnswer answer = SentimentAnswer.builder().answer("It is likely").build();
+		when(magic8BallService.getRandomAnswer(buildMockUserRequest())).thenReturn(answer);
+
+		mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON).content(
+				"{\"question\":\"Will it rain today\",\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
+				.andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void responseAnswerWhenPayLoadContainsNull() throws Exception {
+		mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON).content(
+				"{\"question\":null,\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
+				.andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void responseAnswerWhenPayLoadContainsToomuchLength() throws Exception {
+		mockMvc.perform(post("/api/answer").contentType(MediaType.APPLICATION_JSON).content(
+				"{\"question\":\"Will it rain todayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy?\",\"language\":{\"locale\":\"en-US\",\"code\":\"en-US\",\"name\":\"USA\"},\"userId\":\"dummy\"}"))
+				.andExpect(status().isOk());
+
+	}
 }
