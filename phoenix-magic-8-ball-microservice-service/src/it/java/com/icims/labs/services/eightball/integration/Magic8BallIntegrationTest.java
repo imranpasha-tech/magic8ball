4fe8e61ca9b3966c2c2069f36dded46f35f9b2c5
vtatir <vtatir@icims.com>
2020-08-28 17:00:05 +0530

    fix: added integration test for trending questions

--- phoenix-magic-8-ball-microservice-service/src/it/java/com/icims/labs/services/eightball/integration/Magic8BallIntegrationTest.java
+++ phoenix-magic-8-ball-microservice-service/src/it/java/com/icims/labs/services/eightball/integration/Magic8BallIntegrationTest.java
@@ -94,6 +94,6 @@ public class Magic8BallIntegrationTest extends AbstractDataTestContainer{
 
 	@Test
 	public void fetchErrorWhenLocaleIsInValidWhileFetchingTrendingQuestions() {
-		Assertions.assertThat(repo.getTrendingQuestionsByLanguage("e", PageRequest.of(0, 25))).isNull();
+		Assertions.assertThat(repo.getTrendingQuestionsByLanguage("e", PageRequest.of(0, 25)), is(empty()));
 	}
 }
